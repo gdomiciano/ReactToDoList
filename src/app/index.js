64a274cb90abe31b3f062b4f8e321c79523d3aed
Geisy <geisy@kognity.com>
2018-05-01 17:01:16 +0200

    Create very first component with propertiesz

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,26 @@
-alert('jh');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create Component
+class TodoComponent extends React.Component{
+  render() {
+    return(
+      <div>
+        <h2>{this.props.message}</h2>
+        <p> <strong>Cheese name:</strong> {this.props.cheese.name}</p>
+        <p> <strong>Cheese smell factor:</strong> {this.props.cheese.smellFactor}</p>
+        <p> <strong>Cheese price:</strong> {this.props.cheese.price}</p>
+      </div>
+    );
+  }
+}
+
+const myCheese = {
+  name: 'Camembert',
+  smellFactor: 'Extreme pong',
+  price: '3.50',
+}
+
+//put component into html page
+
+ReactDOM.render(<TodoComponent message="I like cheese" cheese={myCheese} />, document.getElementById('todo-wrapper'));
